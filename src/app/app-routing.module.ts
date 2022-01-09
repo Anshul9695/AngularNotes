9ce8fd84cbbaabe5583be15097e11f69a77cc395
Anshul <anshulpayasi9695@gmail.com>
2022-01-09 22:33:23 +0530

    routing in angular js

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AngularFunctionComponent} from './angular-function/angular-function.component';
+import{ChieldComponent} from './chield/chield.component';
+import {Chield2Component} from './chield2/chield2.component'
+const routes: Routes = [
+  {
+    path:'angularfuncation',
+    component:AngularFunctionComponent
+  },
+  {
+    path:'chield',
+    component:ChieldComponent
 
-const routes: Routes = [];
+  },
+  {
+    path:'chield2',
+    component:Chield2Component
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
